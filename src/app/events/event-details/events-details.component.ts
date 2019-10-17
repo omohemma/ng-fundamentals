2b182d49c8f0a1b42041bee03bb6809c4adb5a9f
Omololu Emmanuel <emmaomoh@gmail.com>
2019-10-17 16:04:31 +0100

    working on event-details page

---
+++ src/app/events/event-details/events-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  templateUrl: './events-details.component.html',
+  styles: [`
+    .container
+  `]
+})
+
+export class EventDetailsComponent {
+
+}
